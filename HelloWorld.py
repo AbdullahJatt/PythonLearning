542339f566a9255cdecf2a7415c8c619d281b7fb
Abdullah Jatt <90279593+AbdullahJatt@users.noreply.github.com>
2021-09-08 02:47:49 -0700

    Add files via upload

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,15 +3,15 @@ print('Abdullah ny akheer kerdi')
 print('O ja o ja')
 print("Hello world")
 #Errors
-# prin("Error")
-# print'error'
-# prit ("error")
-# print(error)
-# print"error"
-# Print ("error")
-# PRINT ("ERROR")
-# prin t ('error')
-# print("error"
-# ("error")print
-# pri('error')nt 
-#print{'error'}
+# prin("Error")         word T is missing
+# print'error'          parenthesis missing
+# prit ("error")        word print wasnot completed
+# print(error)          commas missing
+# print"error"          parenthesis missing
+# Print ("error")       word P is capital
+# PRINT ("ERROR")       whole function is written in capital words
+# prin t ('error')      there is a space b/w function print
+# print("error"         last bracket is missing
+# ("error")print        the function is after parenthesis
+# pri('error')nt        half function is written before parenthesis and half is written after it
+#print{'error'}         use of curly brackets
