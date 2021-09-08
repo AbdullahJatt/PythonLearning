0f3e6b5bee1449c480dfaef9a04b27a92ae55920
Abdullah Jatt <90279593+AbdullahJatt@users.noreply.github.com>
2021-09-08 02:23:16 -0700

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,17 @@
+print('Hello world')
+print('Abdullah ny akheer kerdi')
+print('O ja o ja')
+print("Hello world")
+#Errors
+# prin("Error")
+# print'error'
+# prit ("error")
+# print(error)
+# print"error"
+# Print ("error")
+# PRINT ("ERROR")
+# prin t ('error')
+# print("error"
+# ("error")print
+# pri('error')nt 
+#print{'error'}
