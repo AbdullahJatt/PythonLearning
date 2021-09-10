f9ce19ef219348b45e84a90a53c2b242bbf93a28
Abdullah Jatt <90279593+AbdullahJatt@users.noreply.github.com>
2021-09-10 08:23:33 -0700

    Add files via upload

---
+++ print.py
@@ -0,0 +1,44 @@
+#RULES of assigning variables 
+# some of theme are as following 
+# variables must be letters
+# numbers or digits are not allowed
+# varaibles can be self explainatory
+# fullstop , commas , exclamatery marks , symbols are not allowed insted of underscor(_)
+# spaces are also not allowed 
+#  semicolon , colon , brackests are also not allowed 
+
+#........................................
+
+# data types
+# int , string , float , object , bool , range 
+
+#..........................................
+
+#Print Function
+
+boys = 4
+girls = 7
+teachers = 10
+ratio = 5.2
+first_controller = "Principal"
+sec_controller =  "Voice principal"
+
+
+print (boys+girls)
+print (girls-boys)
+print (boys*girls)
+print (boys/girls)
+print (boys+girls+teachers)
+res = boys+girls-teachers
+print (res)
+boys = girls
+print (boys)
+
+
+print (first_controller,sec_controller)
+
+print(type(boys))
+print(type(girls))
+print(type(ratio))
+print(type(first_controller))
+print(type(sec_controller))
