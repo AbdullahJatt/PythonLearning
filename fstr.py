f3f203eb10396561af907b73e87b5e2bf9dcb5dd
Abdullah Jatt <90279593+AbdullahJatt@users.noreply.github.com>
2021-09-11 20:43:35 +0500

    Add files via upload

---
+++ fstr.py
@@ -0,0 +1,35 @@
+# Print Function
+
+#varaibles
+
+principal = "Abdullah"
+teacher = "Arham Ruimi"
+
+print("Hello everyone my self", principal,"and I'm your principal.", "Meet Mr.",teacher,"he is your python teacher")
+
+# OR
+print(".........................................................................")
+
+print(f"Hello everyone my self {principal} and I'm your principal. Meet Mr.{teacher} he is your python teacher")
+
+print(".........................................................................")
+
+#varaibles
+
+boys = 12
+girls = 17
+teacher = 8
+ratio = 7.4
+principal = "Arham Rumi"
+
+print( f"According to Mr.{principal}. There are {boys} boys and {girls} girls and approximately the ratio of the students is {ratio} who studied from {teacher} teachers." )
+
+print(".........................................................................")
+
+#variables
+first_name = "Abdullah"
+last_name = "Mustafa"
+age = 18
+print( f"My first name is{first_name} and last name is {last_name} and I'm {age}" )
+#Data-Type
+print( f"My first name is {type(first_name)} and last name is {type(last_name)} and I'm {type(age)}" )
