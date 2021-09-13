a26549417a9c40f514becf8e32d0e1b5f22b0854
Abdullah Jatt <90279593+AbdullahJatt@users.noreply.github.com>
2021-09-13 21:08:41 +0500

    Assignment # 4
    Basic Operations

---
+++ operations.py
@@ -0,0 +1,119 @@
+#Basic operations
+# 1) Addition (+)
+# 2) Subraction (-)
+# 3) Multiplication (*)
+# 4) Division (/)
+# 5) Floor Division (//)
+#       Floor division is used to covert float result into int        
+# 6) Exponentiation (**)
+#       Exponentiation is used for supposing Y is the power of X
+# 7) Modulus (%)
+#       Modulus is used to get the remainder of division
+
+#Varaibles
+operande_1 = 5
+operande_2 = 7
+
+# Result = 
+# 1) One operator 
+# 2) Two operands
+
+#Let's try the basic operations
+
+print("-----------------------------------------------")
+
+print(operande_1 + operande_2)
+#12
+
+print("-----------------------------------------------")
+
+print(operande_1 - operande_2)
+#-2
+
+print("-----------------------------------------------")
+
+print(operande_1 * operande_2)
+#35
+
+print("-----------------------------------------------")
+
+print(operande_1 / operande_2)
+#0.7142857142857143
+
+print("-----------------------------------------------")
+
+print(operande_1 // operande_2)
+#0
+
+print("-----------------------------------------------")
+
+print(operande_1 ** operande_2)
+#78125
+
+print("-----------------------------------------------")
+
+print(operande_1 % operande_2)
+#5
+
+print("-----------------------------------------------")
+
+#Calculator
+
+#varaibles
+number1 = 3
+number2 = 8
+
+print("-----------------------------------------------")
+
+res1 = number1 + number2
+print(f"The Addition of number 1 which is {number1} and number 2 which is {number2} = {res1}")
+#3+8=
+#11
+
+print("-----------------------------------------------")
+
+res2 = number1 - number2
+print(f"The Subtraction of number 1 which is {number1} and number 2 which is {number2} = {res2}")
+#3-8=
+#-5
+
+print("-----------------------------------------------")
+
+res3 = number1 * number2
+print(f"The multiplication of number 1 which is {number1} and number 2 which is {number2} = {res3}")
+#3*8=
+#24
+
+print("-----------------------------------------------")
+
+res4 = number1 / number2
+print(f"The division of number 1 which is {number1} and number 2 which is {number2} = {res4}")
+#3/8=
+#0.375
+
+print("-----------------------------------------------")
+
+res5 = number1 // number2
+print(f"The floor division of number 1 which is {number1} and number 2 which is {number2} = {res5}")
+#3//8=
+#0
+
+print("-----------------------------------------------")
+
+res6 = number1 ** number2
+print(f"The exponentiation of number 1 which is {number1} and number 2 which is {number2} = {res6}")
+#3**8=
+#6561
+
+print("-----------------------------------------------")
+
+res7 = number1 % number2
+print(f"The modulus of number 1 which is {number1} and number 2 which is {number2} = {res7}")
+#3%8=
+#3
+
+print("-----------------------------------------------")
+
+print("----------------- THE END ---------------------")
+
+print("-----------------------------------------------")
