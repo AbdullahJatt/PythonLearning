00da1eaaf2313c19ee69f0f454d47baaa2abf9a9
Abdullah Jatt <90279593+AbdullahJatt@users.noreply.github.com>
2021-09-13 07:23:03 -0700

    Update fstr.py

--- fstr.py
+++ fstr.py
@@ -6,11 +6,14 @@ principal = "Abdullah"
 teacher = "Arham Ruimi"
 
 print("Hello everyone my self", principal,"and I'm your principal.", "Meet Mr.",teacher,"he is your python teacher")
+#Hello everyone my self Abdullah and I'm your principal. Meet Mr. Arham Ruimi he is your python teacher
+
 
 # OR
 print(".........................................................................")
 
 print(f"Hello everyone my self {principal} and I'm your principal. Meet Mr.{teacher} he is your python teacher")
+#Hello everyone my self Abdullah and I'm your principal. Meet Mr.Arham Ruimi he is your python teacher
 
 print(".........................................................................")
 
@@ -23,6 +26,7 @@ ratio = 7.4
 principal = "Arham Rumi"
 
 print( f"According to Mr.{principal}. There are {boys} boys and {girls} girls and approximately the ratio of the students is {ratio} who studied from {teacher} teachers." )
+#According to Mr.Arham Rumi. There are 12 boys and 17 girls and approximately the ratio of the students is 7.4 who studied from 8 teachers.
 
 print(".........................................................................")
 
@@ -31,5 +35,8 @@ first_name = "Abdullah"
 last_name = "Mustafa"
 age = 18
 print( f"My first name is{first_name} and last name is {last_name} and I'm {age}" )
+#My first name isAbdullah and last name is Mustafa and I'm 18
+
 #Data-Type
 print( f"My first name is {type(first_name)} and last name is {type(last_name)} and I'm {type(age)}" )
+#My first name is <class 'str'> and last name is <class 'str'> and I'm <class 'int'>
