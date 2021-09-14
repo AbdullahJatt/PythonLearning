ab32df2f1f7242eef9926b6814a659452e897740
Abdullah Jatt <90279593+AbdullahJatt@users.noreply.github.com>
2021-09-14 16:38:21 +0500

    Assignment # 5
    Use of Assignment Operator

---
+++ assignmentOperator.py
@@ -0,0 +1,59 @@
+#Use of Assignment Operator
+#Assignment operator (=)
+
+#Varaibles
+Girls = 12
+Boys = 17
+
+res_1 = Girls + Boys
+print(res_1)
+#29
+
+res = Girls - Boys
+print(res)
+#-5
+
+res = Girls * Boys
+print(res)
+#204
+
+res = Girls / Boys
+print(res)
+#0.7058823529411765
+
+res = Girls ** Boys
+print(res)
+#2218611106740436992
+
+res = Girls // Boys
+print(res)
+#0
+
+res = Girls % Boys
+print(res)
+#12
+
+print(f"Adding boys and girls = {res_1}")
+#29
+
+#Adding values in variables useing assignment operator
+#Method 1
+
+res = Girls + 10
+print(res)
+#22
+
+res = Boys - 7
+print(res)
+#10
+
+#Method 2
+
+teacher = 3
+teacher += 10
+print(teacher)
+#13
+
+Boys += 4
+print(Boys)
+#21
